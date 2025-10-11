bc467950565557773636db3d96a56ba444b6a3a0
Gustavo Assis Santos <gustavo.2083.assis@hotmail.com>
2025-10-10 21:48:54 -0300

    learn(ap07-react): encerra a secao 2.5
        - retomar fazendo a funcao de busca

---
+++ ap07-react-exibe-fotos/src/App.jsx
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+import Busca from './components/Busca'
+
+export default class App extends Component {
+  render() {
+    return (
+
+      <div className='grid w-9 m-auto border-1 border-400'>
+
+        <div className="col-12">
+          <h1>Exibir Lista...</h1>
+        </div>
+
+        <div className="col-12">
+          <Busca dica="Procurar..."/>
+        </div>
+        
+      </div>
+      
+    )
+  }
+}
